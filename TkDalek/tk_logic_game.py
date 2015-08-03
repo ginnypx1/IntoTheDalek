181f11e157b82b3a9649383a5fac2c3d938a205d
ginnypx1 <ginny.pennekamp@gmail.com>
2015-08-03 14:06:54 -0700

    New pieces of the Into the Dalek game
    logic game scenario & crack the code scenario

--- TkDalek/tk_logic_game.py
+++ TkDalek/tk_logic_game.py
@@ -1,43 +1,70 @@
-def logic_quiz():
+from tkinter import *
+
+
+class LogicQuiz(Frame):
 	"""You must solve a simple logic puzzle to discover the good Dalek"""
+	def __init__(self, parent=None):
+		Frame.__init__(self, parent)
 
-	print("The rebel officers step aside.")
-	print("Two Daleks are wheeled into the center of the room.")
-	print("One is the 'good' Dalek. The other is evil.")
-	print("The officers will let you choose one Dalek...")
-	print("But upon doing so you must release it from its chains.")
-	print("\n")
-	print("You can ask one Dalek one question.")
-	print("You assume, as is the case with all Daleks...")
-	print("...that the real Dalek will only lie.")
-	print("The good Dalek must only tell the truth.")
-	print("\n")
+		# create the game directions
+		dirs = """The rebel officers step aside.
+Two Daleks are wheeled into the center of the room.
+One is the 'good' Dalek. The other is evil.
+The officers will let you choose one Dalek...
+But upon doing so you must release it from its chains.
 
-	question = input("What question do you ask? >  ").lower()
+You can ask one Dalek one question.
+You assume, as is the case with all Daleks...
+...that the real Dalek will only lie.
+The good Dalek must only tell the truth.
 
-	if "other" in question:
-		print("-"*20)
-		print("You turn away from that Dalek and point to the other one.")
-		print("You tell the guards to release him. They unlock the chains.")
-		print("You all hold your breath...")
-		print("The Dalek does not kill you.")
-		print("It's a miracle. A good Dalek does exist.")
-		print("\n")
-		print("The Doctor asks, 'Do you have a shrink ray on board?'")
-		print("Journey Blue answers, 'Of course!'")
-		print("The Doctor explains his plan: you'll use the shrink ray.")
-		print("You will journey into the good Dalek's mind.")
-		print("You wait patiently while the shrink ray warms up...")
-		print("5... 4... 3... 2... 1...")
-		print("\n")
-		# proceed to the next room, the Cortex Vault
-		return 'cortex_vault'
-	else:
-		print("-"*20)
-		print("You tell the guards to release the Dalek.")
-		print("They unlock the chains.")
-		print("You all hold your breath...")
-		print("The Dalek opens fire...")
-		print("\n")
-		# play Death scene
-		return 'death'
+What question do you ask?"""
+		logic_dir = Text(self, width=70, height=12, wrap=WORD)
+		logic_dir.insert(1.0, dirs)
+		logic_dir.pack(side=TOP, expand=YES, fill=BOTH)
+
+		# create the input box to answer question
+		logic_var = StringVar()
+		logic_ent = Entry(self, textvariable=logic_var)
+		logic_ent.pack(side=TOP, fill=X)
+
+		# create the button to check the answer
+		logic_but = Button(self, text='Ask', command=(lambda: self.check_answer(logic_var.get(), logic_dir)))
+		logic_but.pack(side=TOP, fill=X)
+
+	# create the function to check the answer, finish the game	
+	def check_answer(self, answer, dir_tag):
+		answer.lower()
+		dir_tag.delete(1.0, END)
+		if "other" in answer:
+			solve_text = """You turn away from that Dalek and point to the other one.
+You tell the guards to release him. They unlock the chains.
+You all hold your breath...
+The Dalek does not kill you.
+It's a miracle. A good Dalek does exist.
+
+The Doctor asks, 'Do you have a shrink ray on board?'
+Journey Blue answers, 'Of course!'
+The Doctor explains his plan: you'll use the shrink ray.
+You will journey into the good Dalek's mind.
+You wait patiently while the shrink ray warms up...
+5... 4... 3... 2... 1..."""
+			dir_tag.insert(1.0, solve_text)
+			# proceed to the next room, the Cortex Vault
+			return 'cortex_vault'
+		else:
+			miss_text = """You tell the guards to release the Dalek.
+They unlock the chains.
+You all hold your breath...
+The Dalek opens fire..."""
+			dir_tag.insert(1.0, miss_text)
+			# play Death scene
+			return 'death'
+
+
+if __name__ == '__main__':
+	root = Tk()
+	root.title('Logic Game')
+	lg = LogicQuiz(root)
+	lg.pack(side=TOP)
+	root.mainloop()
--- TkDalek/tk_vault.py
+++ TkDalek/tk_vault.py
@@ -1,53 +1,82 @@
+from tkinter import *
 from random import randint
 
 
-def crack_code():
+class CrackCode(Frame):
 	"""You must crack a simple code to advance to the next part of the Dalek"""
+	def __init__(self, parent=None):
+		Frame.__init__(self, parent)
+		self.code = "{}{}{}".format(randint(0,1), randint(0,1), randint(0,1))
+		self.count = 0
+		self.guessed_codes = []
 
-	print("\n")
-	print("There's a simple keypad lock on the Dalek's centeral vault.")
-	print("'Good news!' The Doctor says. 'Only 3 digits...")
-	print("...And the Daleks only know 1s and 0s.")
-	print("'This should be fairly easy to crack...'")
-	print("'But we only get 5 tries, before it shuts down for 24 hours.'")
-	print("\n")
+		# create the game directions
+		dirs = """There's a simple keypad lock on the Dalek's centeral vault.
+'Good news!' The Doctor says. 'Only 3 digits...
+...And the Daleks only know 1s and 0s.
+'This should be fairly easy to crack...'
+'But we only get 5 tries, before it shuts down for 24 hours.'"""
+		code_dir = Text(self, width=70, height=12, wrap=WORD)
+		code_dir.insert(1.0, dirs)
+		code_dir.pack(side=TOP, expand=YES, fill=BOTH)
 
-	code = "{}{}{}".format(randint(0,1), randint(0,1), randint(0,1))
-	guess = 222
-	guesses = 0
+		# create the input box to enter a possible code and button to check answer
+		code_frm = Frame(self)
+		code_lbl = Label(code_frm, text='[KEYPAD=>]').pack(side=LEFT)
+		code_var = IntVar()
+		code_ent = Entry(code_frm, textvariable=code_var).pack(side=LEFT)
+		code_but = Button(code_frm, text='Enter', command=(lambda: self.check_code(code_var.get(), code_dir))).pack(side=RIGHT)
+		code_frm.pack(side=TOP, expand=YES, fill=BOTH)
 
-	while guess != code and guesses < 5:
-				
-		guesses += 1
-		guess = input("[keypad]>  ")
-				
-		if guess == code:
-			print("-"*20)
-			print("POP! The Dalek vault opens.")
-			print("Journey Blue steps forward and sticks her hand inside.")
-			print("'Wrong move,' says the Doctor.")
-			print("You watch as the Dalek unleashes an attack force...")
-			print("\n")
-			print("It is a squadron of  super-fast autonomic antibodies.")
-			print("They attack what is giving them pain...") 
-			print("They surround Journey Blue and they EXTERMINATE.")
-			print("The Doctor shouts, 'We have to find somewhere unguareded! Run!'")
-			print("\n")
+	def counter(self):
+		"keeps track of the number of guesses"
+		self.count += 1
+		return self.count
+
+	def check_code(self, guess, dir_tag):
+		"checks the entered code"
+		# count the number of guesses
+		self.guessed_codes.append(str(guess))
+		total = self.counter()
+		dir_tag.delete(1.0, END)
+		# if guess == self.code:
+		if guess == self.code:
+			# display win state
+			win_text = """POP! The Dalek vault opens.
+Journey Blue steps forward and sticks her hand inside.
+'Wrong move,' says the Doctor.
+You watch as the Dalek unleashes an attack force...
+
+It is a squadron of  super-fast autonomic antibodies.
+They attack what is giving them pain...
+They surround Journey Blue and they EXTERMINATE.
+The Doctor shouts, 'We have to find somewhere unguareded! Run!'"""
+			dir_tag.insert(1.0, win_text)
 			# proceed to the next room
-			return 'waste_center'	
-		else:
-			print("BZZZZZZ. Incorrect. Try again. {} guess(es) remaining.".format(5 - int(guesses)))
+			return 'waste_center'
+		elif total == 5:
+			# display loss state
+			loss_text = """Oh no. The Dalek vault shuts down....
+Journey Blue gets out her grapple hook and shoots it.
+'Wrong move,' says the Doctor.
+You watch as the Dalek unleashes an attack force...
+It is a squadron of  super-fast autonomic antibodies.
 
-	if guess != code and guesses == 5:
-		print("-"*20)
-		print("Oh no. The Dalek vault shuts down....")
-		print("Journey Blue gets out her grapple hook and shoots it.")
-		print("'Wrong move,' says the Doctor.")
-		print("You watch as the Dalek unleashes an attack force...")
-		print("It is a squadron of  super-fast autonomic antibodies.")
-		print("\n")
-		print("They attack what is giving them pain...") 
-		print("They surround you all and they EXTERMINATE.")
-		print("\n")
-		# DIE
-		return 'death'
+They attack what is giving them pain... 
+They surround you all and they EXTERMINATE."""
+			dir_tag.insert(1.0, loss_text)
+			# DIE
+			return 'death'
+		else:
+			miss_text = """BZZZZZZ. Incorrect. Try again. {} guess(es) remaining.
+
+You've tried the codes: {}""".format((5 - total), self.guessed_codes)
+			dir_tag.insert(1.0, miss_text)
+
+
+if __name__ == '__main__':
+	root = Tk()
+	root.title('Crack the Code')
+	cc = CrackCode(root)
+	cc.pack(side=TOP)
+	root.mainloop()
