85af3b61b7dd710336c5adda5556c4d43126ad70
Ginny <ginny.pennekamp@gmail.com>
2015-12-04 17:31:44 -0800

    working on waste center

---
+++ js/rps.js
@@ -0,0 +1,58 @@
+// GAME VARIABLES
+
+var winText = "<p>You win!</p><p>No garbage for you.  Unfortunately, that means the second you walk out the door... You are attacked and killed.</p>";
+var loseText = "<p>You lose!</p><p>You take a deep breath....  And cover yourself in garbage.</p><p>You step outside. It works! The Dalek ignores you.  Now... You must follow the source of the radiation...  and stop the leak in order to complete your quest.</p><p>You make a run for the Power Center.</p>";
+var drawText = "<p>It's a draw. Throw again.</p>";
+var choices = ["Rock", "Paper", "Sonic Screwdriver"];
+
+// randomly pick rock paper or scissors
+var randNum = Math.floor((Math.random() * 3) + 1) - 1;
+var drHand = choices[randNum];
+
+// GAME FUNCTIONS
+$("#start").click(function () {
+    $(".game-window").show();
+    $(".entry-text").hide();
+});
+// when next button clicked, move to waste center
+$("#next").click(function () {
+    window.location.href='http://ginnypx1.github.io/IntoTheDalek/power_center.html';
+});
+// when death button clicked, move to death page
+$("#death").click(function () {
+    window.location.href='http://ginnypx1.github.io/IntoTheDalek/death.html';
+}); 
+
+// ROCK PAPER SONIC SCREWDRIVER
+
+// When throw button is clicked
+$("#throw").click(function () {
+    // clear .game-results
+    $(".game-results").text(" ");
+    // grab value from radio form, set as var hand
+
+    // testing
+    console.log(drHand);
+    console.log(hand);
+    // check values for win or loss
+    if (drHand == 'Paper' && hand == 'Sonic Screwdriver') || (drHand == 'Sonic Screwdriver' && hand == 'Rock') || (drHand == 'Rock' && hand == 'Paper') {
+        // redirect to death
+        $("#next").hide();
+        // display winText
+        $(".game-results").prepend(winText);
+        // display drHand vs. hand
+        $(".game-results").prepend("<p>" + drHand + " vs. " + hand + "</p>");
+    } else if (drHand == 'Rock' && hand == 'Sonic Screwdriver') || (drHand == 'Paper' && hand == 'Rock') || (drHand == 'Sonic Screwdriver' && hand == 'Paper') {
+        // redirect to power_center
+        $("#death").hide();
+        // disply loseText
+        $(".game-results").prepend(loseText);
+        // display drHand vs. hand
+        $(".game-results").prepend("<p>" + drHand + " vs. " + hand + "</p>");
+    } else {
+        // display drawText
+        $(".game-results").prepend(drawText);
+        // display drHand vs. hand
+        $(".game-results").prepend("<p>" + drHand + " vs. " + hand + "</p>");
+    }  
+});
